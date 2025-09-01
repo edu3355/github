980e392c57224cdaecc296e08a83e196ce35da9f
edu3355 <e.e.edu3355@gmail.com>
2025-09-01 17:11:17 +0900

    button toggle

--- practice2.js
+++ practice2.js
@@ -1,5 +1,10 @@
 const btn = document.querySelector('#button');
 
 btn.addEventListener("click", () => {
-    alert('button clicked');
+    if (btn.style.backgroundColor === "green") {
+        btn.style.backgroundColor = "red";
+    } else {
+        btn.style.backgroundColor = "green";
+    }
+    console.warn('button clicked');
 });
