69f054495f4f0e5cd66224712b51509209a794d3
edu3355 <e.e.edu3355@gmail.com>
2025-09-05 00:13:17 +0900

    css file added

--- practice2.js
+++ practice2.js
@@ -1,10 +1,13 @@
 const btn = document.querySelector('#button');
+const status = document.querySelector('#status');
 
 btn.addEventListener("click", () => {
     if (btn.style.backgroundColor === "green") {
         btn.style.backgroundColor = "red";
+        status.textContent = "OFF";
     } else {
         btn.style.backgroundColor = "green";
+        status.textContent = "ON";
     }
     console.warn('button clicked');
 });
